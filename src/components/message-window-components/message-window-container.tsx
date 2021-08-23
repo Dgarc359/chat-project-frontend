1f4d79aa9b5f2edcf26f41e67d90239149d8b5b2
Dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2021-08-23 16:33:34 -0400

    intial commit with file structure

---
+++ src/components/message-window-components/message-window-container.tsx
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from "react";
+import MessageWindowPresentation from "./message-window-presentation";
+
+export default function MessageWindowContainer(){
+
+    return(
+        <MessageWindowPresentation/>
+    );
+}
---
+++ src/components/message-window-components/message-window-presentation.tsx
@@ -0,0 +1,8 @@
+import React, {useEffect, useState} from "react";
+
+export default function MessageWindowPresentation(){
+
+    return(
+        <h1>test</h1>
+    )
+}
