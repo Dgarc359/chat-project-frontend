223da26fb677b73a1feab9b09741b12c386259bc
Dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2021-08-23 16:38:48 -0400

    added types to components

--- src/components/message-window-components/message-window-container.tsx
+++ src/components/message-window-components/message-window-container.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import MessageWindowPresentation from "./message-window-presentation";
 
-export default function MessageWindowContainer(){
+export default function MessageWindowContainer() : JSX.Element {
 
     return(
         <MessageWindowPresentation/>
--- src/components/message-window-components/message-window-presentation.tsx
+++ src/components/message-window-components/message-window-presentation.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 
-export default function MessageWindowPresentation(){
+export default function MessageWindowPresentation() : JSX.Element {
 
     return(
         <h1>test</h1>
