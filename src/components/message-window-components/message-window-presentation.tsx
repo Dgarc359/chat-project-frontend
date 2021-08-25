ecd3f730f1d15f5f7e94dee541ea0d3b95eb5d73
Dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2021-08-25 19:00:10 -0400

    conditional styling of messages based on sending user

--- src/components/message-window-components/message-window-presentation.tsx
+++ src/components/message-window-components/message-window-presentation.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import { Table } from "react-bootstrap";
+import { Alert } from "react-bootstrap";
 import Message from "../../models/Message";
 import {Button} from "react-bootstrap";
 import { w3cwebsocket as W3CWebSocket, IMessageEvent } from "websocket";
@@ -27,13 +27,27 @@ export default function MessageWindowPresentation(props: IMessageWindowPresentat
     function handleClick(){
         console.log("click");
 
-        client.send("test");
+        client.send("hello");
     }
 
+    function ternaryRenderBasedOnUser(currentUser : string, user : string | undefined){
+        return (currentUser === user) ? 
+            { margin: "0 5% 20px 40%", ...h6StyleRendered } :
+            { margin: "0 40% 20px 5%", ...h6StyleRendered } ;
+    }
+    
+    const currentUser : string = "david";
+
     return(
-        // <div>
-        //     {props.records.map((data) => <h6>{data.message} - {data.user}</h6>)}
-        // </div>
-        <Button key="Key" onClick={handleClick}>Click Me!</Button>
+        <div>
+            {props.records.map((data) => <h6 key={data.id} style={ternaryRenderBasedOnUser(currentUser, data.user?.toString())}>{data.user}: {data.message}</h6>)}
+        </div>
     );
+}
+
+
+
+const h6StyleRendered = {
+    backgroundColor: "lightgrey",
+    padding: "3px 5% 3px 5%",
 }
